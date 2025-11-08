84c37630fc2c09697df4face66fd5d36cb29985d
StrikerSam-Ai <shashwatmishra0369@gmail.com>
2025-11-09 01:48:57 +0530

    refactor: simplify Graph initialization and remove unused methods

--- core/graph.py
+++ core/graph.py
@@ -3,19 +3,16 @@
 from typing import List, Tuple, Dict
 
 class Graph:
-    def __init__(self, num_nodes: int):
+    def __init__(self, num_nodes):
         self.num_nodes = num_nodes
         self.nodes = list(range(num_nodes))
-        self.adj_list: Dict[int, List[Tuple[int, float]]] = {i: [] for i in range(num_nodes)}
-        self.edges: List[Tuple[int, int, float]] = []
+        self.edges = []
+        self.adj_list = [[] for _ in range(num_nodes)]
 
-    @classmethod
-    def from_edge_list(cls, num_nodes: int, edges: List[Tuple[int, int, float]]):
-        graph = cls(num_nodes)
-        for u, v, w in edges:
-            graph.adj_list[u].append((v, w))
-            graph.edges.append((u, v, w))
-        return graph
+    def add_edge(self, u, v, w=1):
+        self.edges.append((u, v, w))
+        self.adj_list[u].append((v, w))
 
-    def get_neighbors(self, u: int) -> List[Tuple[int, float]]:
+    def get_neighbors(self, u):
         return self.adj_list[u]
+
