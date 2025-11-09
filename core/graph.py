70bdecb1dc0977cb55fe5b85b94b3905f1b906f3
StrikerSam-Ai <shashwatmishra0369@gmail.com>
2025-11-09 16:26:00 +0530

    refactor(graph): update graph initialization and edge management for improved structure and functionality

--- core/graph.py
+++ core/graph.py
@@ -1,28 +1,30 @@
 # core/graph.py
 
 class Graph:
-    def __init__(self):
-        self.nodes = set()
-        self.edges = []
-        self.adj_list = {}
+    def __init__(self, num_nodes: int):
+        self.num_nodes = num_nodes
+        self.adj_list = {i: [] for i in range(num_nodes)}  # adjacency list
 
-    def add_node(self, node):
-        if node not in self.nodes:
-            self.nodes.add(node)
-            self.adj_list[node] = []
+    @property
+    def nodes(self):
+        """BMSSP ke liye node list return karta hai"""
+        return list(self.adj_list.keys())
 
-    def add_edge(self, src, dst, weight=1):
-        if src in self.nodes and dst in self.nodes:
-            self.edges.append((src, dst, weight))
-            self.adj_list[src].append((dst, weight))
+    def add_edge(self, u, v, w=1):
+        """Undirected weighted graph"""
+        self.adj_list[u].append((v, w))
+        self.adj_list[v].append((u, w))
+
+    def neighbors(self, node):
+        """Return neighbors like: [(nbr, weight), ...]"""
+        return self.adj_list[node]
+
+    def get_neighbors(self, node):
+        """BMSSP ke compatible naming ke liye wrapper"""
+        return self.neighbors(node)
 
     def remove_node(self, node):
-        if node in self.nodes:
-            self.nodes.remove(node)
-
-        if node in self.adj_list:
-            del self.adj_list[node]
-
-        # Remove edges pointing to removed node
-        for src in list(self.adj_list.keys()):
-            self.adj_list[src] = [(dst, w) for dst, w in self.adj_list[src] if dst != node]
+        """Node remove + edges cleanup"""
+        for key in list(self.adj_list.keys()):
+            self.adj_list[key] = [(n, w) for (n, w) in self.adj_list[key] if n != node]
+        self.adj_list.pop(node, None)
